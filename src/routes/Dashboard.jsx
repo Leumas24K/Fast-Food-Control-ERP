c9ce36d8c3dbf2d72c3374122bfd8b7ee47194fd
Samuel Sanchez <samueljsanchez903@gmail.com>
2026-01-28 22:47:37 -0400

    cambiando cositas

--- src/routes/Dashboard.jsx
+++ src/routes/Dashboard.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Dashboard = () => {
   return (
     <div>
-      <h1 className='text-center'>vista del Dashboard</h1>
+        <h1>vista del Dashboard</h1>
     </div>
   )
 }
